d6613fafeb81dd998870e9e9801ba5fb4e8a130a
Woinshet <woinshet1>
2022-05-08 01:47:47 -0700

    assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(event) {
+
+  event.preventDefault();
+
+  // preparing/setting up the task list
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = (event.target.description.value);
+  taskList.append(task);
+
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.addEventListener('click', () => {
+    taskList.removeChild(task);
+    //or task.remove();
+  });
+  task.append(deleteButton);
+
+  // clearing out the text
+  event.target.reset();
+  
+}
